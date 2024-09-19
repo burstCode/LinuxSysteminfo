e8995403b9c9f78c943796eaf6f2bf070ba66e45
burstCode <yourewelcomex@gmail.com>
2024-09-19 23:08:25 +0300

    controversial idea

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import system_info
 
 
 if __name__ == "__main__":
+    info_methods = [
+        system_info.get_dist_info(),
+        system_info.get_cpu_info(),
+        system_info.get_memory_info(),
+        system_info.get_disk_usage()
+    ]
+
+    for method in info_methods:
+        print(method)
+
+    ''' Имеет ли смысл делать так, как это сделано выше? :DDD
+    print(system_info.get_dist_info())
     print(system_info.get_cpu_info())
     print(system_info.get_memory_info())
     print(system_info.get_disk_usage())
+    '''
